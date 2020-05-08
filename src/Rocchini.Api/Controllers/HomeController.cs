d6209868f4a82d9e7aac4315596f8ffdce17b360
dirocchini <roquinni@gmail.com>
2020-05-08 16:21:33 -0300

    adding api basic methods:

---
+++ src/Rocchini.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rocchini.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Get() => Content("Hello from Rocchini Api");
+    }
+}
---
+++ src/Rocchini.Api/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocchini.Api.Controllers
+{
+    public class UserController
+    {
+    }
+}
