6f2c4c15ab10824e908afa596f4b7bc9cc8a6645
dirocchini <roquinni@gmail.com>
2020-05-09 14:33:02 -0300

    implementing service

---
+++ src/Rocchini.Services.Activities/Services/ActivityService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocchini.Services.Activities.Services
+{
+    public class ActivityService
+    {
+    }
+}
