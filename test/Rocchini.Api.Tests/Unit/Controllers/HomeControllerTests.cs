c08a7d3223d3a9f58ad0065c51c64b193e80b53f
dirocchini <roquinni@gmail.com>
2020-05-10 10:53:10 -0300

    testing api

---
+++ test/Rocchini.Api.Tests/Unit/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Rocchini.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Rocchini.Api.Tests.Unit.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void home_controller_get_should_return_string()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Get();
+
+            var contentResult = result as ContentResult;
+            contentResult.Should().NotBeNull();
+            contentResult.Content.ShouldAllBeEquivalentTo("Hello from Rocchini Api");
+        }
+    }
+}
